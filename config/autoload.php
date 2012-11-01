577f7adc2a186042dd7279c45bd6c63934f15966
Olivier Laviale <contact@weirdog.com>
2012-11-01 12:23:45 +0100

    Added Prototype to definitions

--- config/autoload.php
+++ config/autoload.php
@@ -7,5 +7,6 @@ return array
 	'ICanBoogie\PropertyNotDefined' => $path . 'lib/object.php',
 	'ICanBoogie\PropertyNotReadable' => $path . 'lib/object.php',
 	'ICanBoogie\PropertyNotWritable' => $path . 'lib/object.php',
-	'ICanBoogie\PropertyIsReserved' => $path . 'lib/object.php'
+	'ICanBoogie\PropertyIsReserved' => $path . 'lib/object.php',
+	'ICanBoogie\Prototype' => $path . 'lib/prototype.php'
 );
