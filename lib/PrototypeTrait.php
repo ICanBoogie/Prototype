ec38d8b214a20bfe3e702eb2b7c7b19d73d59aea
Olivier Laviale <olivier.laviale@gmail.com>
2023-03-05 02:52:37 +0100

    Add Prototype::has_method()

--- lib/PrototypeTrait.php
+++ lib/PrototypeTrait.php
@@ -107,9 +107,7 @@ trait PrototypeTrait
             return true;
         }
 
-        $prototype = $this->prototype ?? $this->get_prototype();
-
-        return isset($prototype[$method]);
+        return Prototype::has_method($this, $method);
     }
 
     /**
