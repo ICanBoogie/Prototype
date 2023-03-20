49643ad2b078ab0bdb44887724fa11638a4cf6f9
Olivier Laviale <olivier.laviale@gmail.com>
2023-03-21 00:00:17 +0100

    Remove AllowDynamicProperties from Prototyped

--- tests/PrototypedCases/SampleA.php
+++ tests/PrototypedCases/SampleA.php
@@ -2,8 +2,10 @@
 
 namespace Test\ICanBoogie\PrototypedCases;
 
+use AllowDynamicProperties;
 use ICanBoogie\Prototyped;
 
+#[AllowDynamicProperties]
 class SampleA extends Prototyped
 {
     public int $seconds = 0;
